13cd2347e8653b62d4cf40f52ec79707a8a6f142
Felice Forby <feliceforby@gmail.com>
2018-12-29 00:57:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee #${i + 1}.`);
+  }
+
+  return employees;
+}
+
+function flipCoin() {
+  if (Math.random() >= 0.5) {
+    return "Tails";
+  } else {
+    return "Heads";
+  }
+}
+
+function tailsNeverFails() {
+  let tailsFlips = 0;
+
+  while ( flipCoin() == 'Tails' ) {
+    tailsFlips++;
+  }
+
+  return `You got ${tailsFlips} tails in a row!`;
+}
