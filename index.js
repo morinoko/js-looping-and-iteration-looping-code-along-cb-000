960d69023b3b06bb323df12e142d864f6643ce54
Felice Forby <feliceforby@gmail.com>
2018-12-29 00:31:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solutions in this file
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++) {
+    console.log(`Welcome ${employees[i]}! You are employee ${i + 1}!`);
+  }
+
+  return employees;
+}
